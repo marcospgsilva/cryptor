8e0a77dd4fb3eeb55023e726e20e7d8ae33546f8
Marcos Paulo <marcos@meucredere.com.br>
2021-08-26 22:54:37 -0300

    releases configuration

--- config/releases.exs
+++ config/releases.exs
@@ -1,1 +1,6 @@
 import Config
+
+config :cryptor, CryptorWeb.Endpoint,
+  server: true,
+  http: [port: {:system, "PORT"}],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
