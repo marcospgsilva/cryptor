53a2ba1cbeab06c416f19d8c7d2db23050868f9b
Marcos Paulo <marcos@meucredere.com.br>
2021-08-28 12:22:15 -0300

    Add update state schedule event

--- lib/cryptor_web/live/orders_live.ex
+++ lib/cryptor_web/live/orders_live.ex
@@ -6,34 +6,57 @@ defmodule CryptorWeb.OrdersLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    %{order_list: order_list} = :sys.get_state(TradeServer)
-
-    case order_list do
+    case render_orders() do
       [] ->
+        schedule_event()
+
         {:ok,
          assign(socket,
            orders: [
              %{
-               order_id: 1,
                coin: "Teste",
-               quantity: 1.00,
-               finished: false,
-               type: "buy"
+               price: 1.00,
+               current_value: false,
+               variation: 1.2
              }
            ]
          )}
 
-      order_list ->
-        {:ok, assign(socket, orders: order_list)}
+      orders ->
+        schedule_event()
+        {:ok, assign(socket, orders: orders)}
     end
   end
 
   @impl true
-  def handle_event("update_state", _value, socket) do
+  def handle_info("update_state", socket) do
     IO.inspect("UPDATE_STATE")
-    %{order_list: order_list} = :sys.get_state(TradeServer)
-    Process.send_after(self(), "update_state", 3000)
-
-    {:noreply, assign(socket, orders: order_list)}
+    schedule_event()
+    {:noreply, assign(socket, orders: render_orders())}
   end
+
+  def render_orders() do
+    %{pid_list: pid_list} = :sys.get_state(TradeServer)
+
+    pid_list
+    |> Enum.map(fn pid ->
+      %{orders: orders, current_value: current_value} = :sys.get_state(pid)
+
+      orders
+      |> Enum.map(fn order ->
+        %{
+          coin: order.coin,
+          price: order.price,
+          current_value: current_value,
+          variation: calculate_variation(order.price, current_value)
+        }
+      end)
+    end)
+    |> Enum.concat()
+  end
+
+  defp schedule_event(), do: Process.send_after(self(), "update_state", 3000)
+
+  defp calculate_variation(bought_price, current_price),
+    do: (current_price / bought_price - 1) |> Float.round(8)
 end
