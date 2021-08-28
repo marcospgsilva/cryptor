9682fede30088937669acd58dcf074e2731854dd
Marcos Paulo <marcos@meucredere.com.br>
2021-08-28 17:57:28 -0300

    Update home styles

---
+++ assets/tailwind.config.js
@@ -0,0 +1,34 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: [
+      "../lib/**/*.eex",
+      "../lib/**/*.leex",
+      "../lib/**/*_view.ex"
+    ],
+    options: {
+      whitelist: [/phx/, /nprogress/]
+    }
+  },
+  theme: {
+    extend: {
+      colors: {
+        'light-blue': colors.lightBlue,
+        cyan: colors.cyan,
+      },
+    },
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'primary': '#181B23',
+      'secondary': '#25262E'
+    })
+  },
+  variants: {
+    extend: {
+      borderWidth: ['hover'],
+    }
+  },
+  plugins: [require('@tailwindcss/forms')],
+}
