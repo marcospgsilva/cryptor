a0a161074123f6c009a85a5dab886d5042a144c1
Marcos Paulo <marcos@meucredere.com.br>
2021-08-28 11:26:45 -0300

    Remove initial orders

--- lib/cryptor/trader/server.ex
+++ lib/cryptor/trader/server.ex
@@ -10,47 +10,16 @@ defmodule Cryptor.Trader.Server do
 
   @currencies ["LTC", "XRP", "ETH", "USDC", "AXS", "BAT", "ENJ", "CHZ"]
 
-  @orders [
-    %{
-      order_id: 1,
-      coin: "XRP",
-      quantity: 1.96132719,
-      price: 6.57033,
-      type: "buy"
-    },
-    %{
-      order_id: 2,
-      coin: "ETH",
-      quantity: 0.00109722,
-      price: 18_100.29,
-      type: "buy"
-    },
-    %{order_id: 3, coin: "BAT", quantity: 10.43067227, price: 4.77, type: "buy"},
-    %{order_id: 4, coin: "CHZ", quantity: 4.80037127, price: 2.06, type: "buy"}
-  ]
-
   def start_link(_attrs) do
     GenServer.start_link(
       __MODULE__,
       %{
         pid_list: [],
-        order_list: get_orders(),
-        # order_list: Order.get_orders(),
+        order_list: Order.get_orders(),
         account_info: Trader.get_account_info()
       },
       name: TradeServer
     )
-  end
-
-  def get_orders() do
-    case Order.get_orders() do
-      [] ->
-        @orders
-        |> Enum.each(&Order.create_order/1)
-
-      orders ->
-        orders
-    end
   end
 
   def add_order(order) do
