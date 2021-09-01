bb36e78ef0109f844f800208a3aeeb44898d07b8
Marcos Paulo <marcos@meucredere.com.br>
2021-08-31 22:01:31 -0300

    Fix AXS buy order match

--- lib/cryptor/trader/amount_control.ex
+++ lib/cryptor/trader/amount_control.ex
@@ -33,6 +33,10 @@ defmodule Cryptor.Trader.AmountControl do
   def get_quantity(:buy, _newer_price, %Order{coin: "BAT"}),
     do: nil
 
+  def get_quantity(:buy, newer_price, %Order{coin: "AXS"}),
+    do: newer_price / 50
+
+
   def get_quantity(:buy, _newer_price, %Order{quantity: 0.00000000, coin: coin}) do
     case coin do
       "ETH" -> @eth_minimum_value
