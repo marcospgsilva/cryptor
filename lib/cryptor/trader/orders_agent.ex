a6b785e278ae7992c461ab64addcf3bd6b15d9fe
Marcos Paulo <marcos@meucredere.com.br>
2021-10-11 12:36:38 -0300

    Refactor orders management

---
+++ lib/cryptor/trader/orders_agent.ex
@@ -0,0 +1,26 @@
+defmodule Cryptor.Trader.OrdersAgent do
+  @moduledoc """
+  Orders Agent
+  """
+  use Agent
+  alias Cryptor.Order
+
+  def start_link(_), do: Agent.start_link(fn -> Order.get_orders() end, name: __MODULE__)
+
+  @spec get_order_list() :: list(map())
+  def get_order_list, do: Agent.get(__MODULE__, & &1)
+
+  @spec add_to_order_list(new_order :: map()) :: list(map())
+  def add_to_order_list(new_order) do
+    Agent.get_and_update(__MODULE__, fn orders ->
+      {orders, [new_order | orders]}
+    end)
+  end
+
+  @spec remove_from_order_list(new_order :: map()) :: list(map())
+  def remove_from_order_list(order_to_be_removed) do
+    Agent.get_and_update(__MODULE__, fn orders ->
+      {orders, List.delete(orders, order_to_be_removed)}
+    end)
+  end
+end
--- lib/cryptor_web/controllers/analysis_controller.ex
+++ lib/cryptor_web/controllers/analysis_controller.ex
@@ -1,8 +1,11 @@
 defmodule CryptorWeb.AnalysisController do
   use CryptorWeb, :controller
+  alias Cryptor.Trader.OrdersAgent
 
   def index(conn, _params) do
-    %{pid_list: pid_list, order_list: order_list} = :sys.get_state(TradeServer)
+    %{pid_list: pid_list} = :sys.get_state(TradeServer)
+    order_list = OrdersAgent.get_order_list()
+
     render(conn, "analysis.json", %{analysis: %{pid_list: pid_list, order_list: order_list}})
   end
 end
