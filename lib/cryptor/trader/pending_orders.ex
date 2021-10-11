efda78adb69ce80ff83028d0d1e24a2534746e81
Marcos Paulo <marcos@meucredere.com.br>
2021-10-11 10:35:11 -0300

    Hotfix: pending orders PID

--- lib/cryptor/trader/pending_orders.ex
+++ lib/cryptor/trader/pending_orders.ex
@@ -7,18 +7,18 @@ defmodule Cryptor.Trader.PendingOrders do
   def start_link(_), do: Agent.start_link(fn -> [] end, name: PendingOrdersServer)
 
   @spec get_pending_orders_list() :: list(map())
-  def get_pending_orders_list, do: Agent.get(__MODULE__, & &1)
+  def get_pending_orders_list, do: Agent.get(self(), & &1)
 
   @spec add_to_pending_orders_list(new_order :: map()) :: list(map())
   def add_to_pending_orders_list(new_order) do
-    Agent.get_and_update(__MODULE__, fn pending_orders ->
+    Agent.get_and_update(self(), fn pending_orders ->
       {pending_orders, [new_order | pending_orders]}
     end)
   end
 
   @spec remove_from_pending_orders_list(new_order :: map()) :: list(map())
   def remove_from_pending_orders_list(order_to_be_removed) do
-    Agent.get_and_update(__MODULE__, fn pending_orders ->
+    Agent.get_and_update(self(), fn pending_orders ->
       {pending_orders, List.delete(pending_orders, order_to_be_removed)}
     end)
   end
